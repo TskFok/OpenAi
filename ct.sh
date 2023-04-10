e405f822a5a4b23db21ce8d60dcdf409b671572d
ushopal <yi.yang@ushopal.com>
2023-04-10 13:51:51 +0800

    重启脚本

--- ct.sh
+++ ct.sh
@@ -3,13 +3,12 @@
 PIDS=`ps -ef |grep chat |grep -v grep | awk '{print $2}'`
 echo $PIDS
 if [ "$PIDS" != "" ]; then
-
 echo "chat is runing!"
+sudo kill -2 $PIDS
+sudo ./chat bg
 
 else
-
 cd /home/yy
-
-./chat bg
+sudo ./chat bg
 
 fi
